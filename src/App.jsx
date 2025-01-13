80043101627e79fa814aba3e202a063df6e38348
Nitramcr <martingolramirez@gmail.com>
2025-01-13 20:34:05 -0300

    Added QR code and footer marquee

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Marquee from 'react-fast-marquee'
 import './App.css'
 import grainedImage from '/grained.png'
 
@@ -15,7 +16,8 @@ export function App() {
         }}
       />
 
-      <div className='relative flex flex-col h-screen'>
+      <section className='relative flex flex-col h-screen'>
+        {/* Description */}
         <aside className='w-[75%] flex items-start z-20'>
           <p className='p-3 font-mono text-pretty'>
             I&apos;m a versatile Software Developer and Certified Cloud
@@ -28,15 +30,27 @@ export function App() {
           </p>
         </aside>
 
+        {/* Hammer */}
         <div className='absolute top-0 right-0 z-10 p-1'>
           <img src='/hammer.gif' alt='' className='size-16 -rotate-[23deg]' />
         </div>
 
+        {/* Works folder */}
         <div className='absolute top-[420px] right-28 z-20 flex flex-col items-center cursor-pointer hover:scale-105 transition-transform duration-100'>
           <img src='/folder.png' alt='' className='w-20 h-16' />
           <p>works</p>
         </div>
 
+        {/* QR Code */}
+        <div className='absolute top-[120px] right-48 z-10'>
+          <img
+            src='/QR-certification.png'
+            alt='QR code for GCP certification'
+            className='size-48 rounded-3xl rotate-[8deg]'
+          />
+        </div>
+
+        {/* Name & role */}
         <div className='absolute inset-0 flex items-center justify-start pb-24 pl-44 z-10'>
           <div className='text-center'>
             <p className='text-red-500 font-semibold text-7xl'>
@@ -47,7 +61,25 @@ export function App() {
             </h1>
           </div>
         </div>
-      </div>
+      </section>
+
+      <footer className='fixed bottom-0 z-20 left-0 right-0 border-t-[3px] border-red-500'>
+        <Marquee
+          speed={55}
+          gradient={false}
+          pauseOnHover={false}
+          className='py-5'
+        >
+          <div className='flex flex-row gap-x-12 font-semibold text-lg'>
+            <span>React</span>
+            <span>Node</span>
+            <span>JavaScript</span>
+            <span>MongoDB</span>
+            <span>Tailwindcss</span>
+            <span>Google Cloud</span>
+          </div>
+        </Marquee>
+      </footer>
     </main>
   )
 }

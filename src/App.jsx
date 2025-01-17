57f8b335dcfa3be4dd27b3e28759aeaebc061820
Nitramcr <martingolramirez@gmail.com>
2025-01-16 22:19:23 -0300

    Finished the footer and marquee with technologies icons.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Marquee from 'react-fast-marquee'
+import 'boxicons'
 import './App.css'
 import grainedImage from '/grained.png'
 
@@ -63,7 +64,7 @@ export function App() {
         </div>
       </section>
 
-      <footer className='fixed bottom-0 z-20 left-0 right-0 border-t-[3px] border-red-500'>
+      <footer className='fixed bottom-0 z-20 left-0 right-0 border-t-[3px] border-black'>
         <Marquee
           speed={55}
           gradient={false}
@@ -71,12 +72,110 @@ export function App() {
           className='py-5'
         >
           <div className='flex flex-row gap-x-12 font-semibold text-lg'>
-            <span>React</span>
-            <span>Node</span>
-            <span>JavaScript</span>
-            <span>MongoDB</span>
-            <span>Tailwindcss</span>
-            <span>Google Cloud</span>
+            <box-icon
+              type='logo'
+              name='html5'
+              size='lg'
+              alt='HTML5 icon'
+            ></box-icon>
+            <box-icon
+              type='logo'
+              name='css3'
+              size='lg'
+              alt='CSS3 icon'
+            ></box-icon>
+            <box-icon
+              type='logo'
+              name='tailwind-css'
+              size='lg'
+              alt='Tailwind CSS icon'
+            ></box-icon>
+            <box-icon
+              type='logo'
+              name='javascript'
+              size='lg'
+              alt='JavaScript icon'
+            ></box-icon>
+            <box-icon
+              type='logo'
+              name='typescript'
+              size='lg'
+              alt='TypeScript icon'
+            ></box-icon>
+            <box-icon
+              type='logo'
+              name='react'
+              size='lg'
+              alt='React icon'
+            ></box-icon>
+            <box-icon
+              type='logo'
+              name='redux'
+              size='lg'
+              alt='Redux icon'
+            ></box-icon>
+            <box-icon
+              type='logo'
+              name='nodejs'
+              size='lg'
+              alt='Node.js icon'
+            ></box-icon>
+            <box-icon
+              type='logo'
+              name='python'
+              size='lg'
+              alt='Python icon'
+            ></box-icon>
+            <box-icon type='logo' name='git' size='lg' alt='Git'></box-icon>
+            <box-icon
+              type='logo'
+              name='github'
+              size='lg'
+              alt='GitHub icon'
+            ></box-icon>
+            <box-icon
+              type='logo'
+              name='mongodb'
+              size='lg'
+              alt='MongoDB icon'
+            ></box-icon>
+            <box-icon
+              type='logo'
+              name='postgresql'
+              size='lg'
+              alt='PostgreSQL icon'
+            ></box-icon>
+            <box-icon
+              type='logo'
+              name='google-cloud'
+              size='lg'
+              alt='Google Cloud icon'
+            ></box-icon>
+            <box-icon
+              type='logo'
+              name='firebase'
+              size='lg'
+              alt='Firebase icon'
+            ></box-icon>
+            <box-icon type='logo' name='aws' size='lg' alt='AWS'></box-icon>
+            <box-icon
+              type='logo'
+              name='docker'
+              size='lg'
+              alt='Docker icon'
+            ></box-icon>
+            <box-icon
+              type='logo'
+              name='kubernetes'
+              size='lg'
+              alt='Kubernetes icon'
+            ></box-icon>
+
+            <img
+              src='./terraform.png'
+              alt='Terraform icon'
+              className='size-11 pt-0.5 mr-11'
+            />
           </div>
         </Marquee>
       </footer>
